4081c2a9c42ced177fab9328f51b74e68e792faf
Ajay Raj <ajay.n.raj@gmail.com>
2023-02-25 22:21:33 -0800

    some minor refactoring

--- src/hooks/conversation.ts
+++ src/hooks/conversation.ts
@@ -23,6 +23,7 @@ export const useConversation = (
   config: ConversationConfig
 ): [symbol, () => void, () => void, Buffer] => {
   const [audioContext, setAudioContext] = React.useState<AudioContext>();
+  const [audioAnalyser, setAudioAnalyser] = React.useState<AnalyserNode>();
   const [audioQueue, setAudioQueue] = React.useState<Buffer[]>([]);
   const [currentAudioBuffer, setCurrentAudioBuffer] = React.useState<Buffer>(
     Buffer.from([0])
@@ -34,23 +35,22 @@ export const useConversation = (
   const [audioMetadata, setAudioMetadata] = React.useState<AudioMetadata>();
   const [status, setStatus] = React.useState(ConversationStatus.IDLE);
 
+  // get audio context and metadata about user audio
   React.useEffect(() => {
-    setAudioContext(new window.AudioContext());
+    const audioContext = new window.AudioContext();
+    setAudioContext(audioContext);
+    setAudioAnalyser(audioContext.createAnalyser());
+    let samplingRate = audioContext.sampleRate;
+    let audioEncoding = "linear16" as AudioEncoding;
+    console.log({ samplingRate, audioEncoding });
+    setAudioMetadata({ samplingRate, audioEncoding });
   }, []);
 
-  React.useEffect(() => {
-    const registerWav = async () => {
-      await register(await connect());
-    };
-    registerWav().catch(console.error);
-  }, []);
-
+  // once the conversation is connected, stream the microphone audio into the socket
   React.useEffect(() => {
     if (!recorder || !socket) return;
-    if (status === ConversationStatus.CONNECTING) {
-      recorder.start(10);
-    } else if (status === ConversationStatus.CONNECTED) {
-      recorder.addEventListener("dataavailable", ({ data }: { data: Blob }) => {
+    if (status === ConversationStatus.CONNECTED) {
+      recorder.ondataavailable = ({ data }: { data: Blob }) => {
         blobToBase64(data).then((base64Encoded: string | null) => {
           if (!base64Encoded) return;
           const audioMessage: AudioMessage = {
@@ -60,70 +60,19 @@ export const useConversation = (
           socket.readyState === WebSocket.OPEN &&
             socket.send(stringify(audioMessage));
         });
-      });
-    }
-  }, [recorder, status]);
-
-  React.useEffect(() => {
-    if (!audioContext) return;
-    let samplingRate = audioContext.sampleRate;
-    let audioEncoding = "linear16" as AudioEncoding;
-    console.log({ samplingRate, audioEncoding });
-    setAudioMetadata({ samplingRate, audioEncoding });
-  }, [audioContext]);
-
-  React.useEffect(() => {
-    if (!socket) return;
-    socket.onerror = (error) => {
-      setStatus(ConversationStatus.ERROR);
-    };
-    socket.onmessage = (event) => {
-      const message = JSON.parse(event.data);
-      if (message.type === "audio") {
-        queueAudio(Buffer.from(message.data, "base64"));
-      } else if (message.type === "ready") {
-        setStatus(ConversationStatus.CONNECTED);
-      }
-    };
-    socket.onclose = () => {
-      stopConversation();
-    };
-  }, [socket]);
-
-  React.useEffect(() => {
-    const awaitSocketAndSendAudioMetadata = async (
-      socket: WebSocket,
-      audioMetadata: AudioMetadata
-    ) => {
-      await new Promise((resolve) => {
-        const interval = setInterval(() => {
-          if (socket.readyState === WebSocket.OPEN) {
-            clearInterval(interval);
-            resolve(null);
-          }
-        }, 100);
-      });
-      const startMessage: StartMessage = {
-        type: "start",
-        transcriberConfig: Object.assign(
-          config.transcriberConfig,
-          audioMetadata
-        ),
-        agentConfig: config.agentConfig,
-        synthesizerConfig: Object.assign(
-          config.synthesizerConfig,
-          audioMetadata
-        ),
       };
-      socket.send(stringify(startMessage));
-    };
-    if (audioMetadata && socket && status === ConversationStatus.CONNECTING) {
-      awaitSocketAndSendAudioMetadata(socket, audioMetadata).catch(
-        console.error
-      );
     }
-  }, [socket, audioMetadata, status]);
+  }, [recorder, socket, status]);
 
+  // accept wav audio from webpage
+  React.useEffect(() => {
+    const registerWav = async () => {
+      await register(await connect());
+    };
+    registerWav().catch(console.error);
+  }, []);
+
+  // when audio comes into the queue, play it to the user
   React.useEffect(() => {
     const playArrayBuffer = (arrayBuffer: ArrayBuffer) => {
       audioContext &&
@@ -148,34 +97,6 @@ export const useConversation = (
     }
   }, [audioQueue, processing]);
 
-  const queueAudio = (audio: Buffer) => {
-    setCurrentAudioBuffer(audio);
-    setAudioQueue((prev) => [...prev, audio]);
-  };
-
-  const streamMicrophoneAudioToSocket = async () => {
-    let audioStreamToUse;
-
-    if (!audioStream) {
-      try {
-        audioStreamToUse = await navigator.mediaDevices.getUserMedia({
-          video: false,
-          audio: true,
-        });
-        setAudioStream(audioStreamToUse);
-        console.log("Access to microphone granted");
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      audioStreamToUse = audioStream;
-    }
-    audioStreamToUse &&
-      setRecorder(
-        new MediaRecorder(audioStreamToUse, { mimeType: "audio/wav" })
-      );
-  };
-
   const stopConversation = () => {
     setAudioQueue([]);
     setStatus(ConversationStatus.IDLE);
@@ -190,9 +111,12 @@ export const useConversation = (
   };
 
   const startConversation = async () => {
-    if (!audioContext) return;
+    if (!audioContext || !audioAnalyser) return;
     setStatus(ConversationStatus.CONNECTING);
-    audioContext.resume();
+
+    if (audioContext.state === "suspended") {
+      audioContext.resume();
+    }
 
     const resp = await fetch(
       `https://${process.env.REACT_APP_BACKEND_URL}/auth/token`,
@@ -207,12 +131,68 @@ export const useConversation = (
     const data = await resp.json();
     const token = data.token;
 
-    const newSocket = new WebSocket(
+    const socket = new WebSocket(
       `wss://${process.env.REACT_APP_BACKEND_URL}/conversation?key=${token}`
     );
-    setSocket(newSocket);
+    socket.onerror = (error) => {
+      setStatus(ConversationStatus.ERROR);
+    };
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === "audio") {
+        const audio = Buffer.from(message.data, "base64");
+        setCurrentAudioBuffer(audio);
+        setAudioQueue((prev) => [...prev, audio]);
+      } else if (message.type === "ready") {
+        setStatus(ConversationStatus.CONNECTED);
+      }
+    };
+    socket.onclose = () => {
+      stopConversation();
+    };
+    setSocket(socket);
 
-    await streamMicrophoneAudioToSocket();
+    // wait for socket to be ready
+    await new Promise((resolve) => {
+      const interval = setInterval(() => {
+        if (socket.readyState === WebSocket.OPEN) {
+          clearInterval(interval);
+          resolve(null);
+        }
+      }, 100);
+    });
+
+    const startMessage: StartMessage = {
+      type: "start",
+      transcriberConfig: Object.assign(config.transcriberConfig, audioMetadata),
+      agentConfig: config.agentConfig,
+      synthesizerConfig: Object.assign(config.synthesizerConfig, audioMetadata),
+    };
+    socket.send(stringify(startMessage));
+
+    const audioStream = await navigator.mediaDevices.getUserMedia({
+      video: false,
+      audio: {
+        echoCancellation: true,
+        autoGainControl: true,
+      },
+    });
+    setAudioStream(audioStream);
+    console.log("Access to microphone granted");
+
+    let recorderToUse = recorder;
+    if (recorderToUse && recorderToUse.state === "paused") {
+      recorderToUse.resume();
+    } else if (!recorderToUse) {
+      recorderToUse = new MediaRecorder(audioStream, {
+        mimeType: "audio/wav",
+      });
+      setRecorder(recorderToUse);
+    }
+
+    recorderToUse.start(10);
+    const source = audioContext.createMediaStreamSource(audioStream);
+    source.connect(audioAnalyser);
   };
 
   return [status, startConversation, stopConversation, currentAudioBuffer];
